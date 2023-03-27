5f785fdec7033767b92439cff391867174674520
likhith_harish <62323090+likhithharish@users.noreply.github.com>
2023-03-27 16:24:20 -0400

    Added my project

---
+++ app/src/main/java/com/example/notetakingapp/NoteApp.kt
@@ -0,0 +1,6 @@
+package com.example.notetakingapp
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class NoteApp : Application()
---
+++ app/src/main/java/com/example/notetakingapp/feature_note/presentation/add_edit_note/NoteTextFieldState.kt
@@ -0,0 +1,8 @@
+package com.example.notetakingapp.feature_note.presentation.add_edit_note
+
+
+data class NoteTextFieldState(
+    val text: String = "",
+    val hint: String = "",
+    val isHintVisible: Boolean = true
+)
